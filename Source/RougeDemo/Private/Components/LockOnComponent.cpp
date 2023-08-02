f4f914df93616808d4d5ef502a6521387944cd85
Alien <767808421@qq.com>
2023-08-03 00:18:12 +0800

    添加锁敌后，转换为二维移动，但镜头未跟随

--- Source/RougeDemo/Private/Components/LockOnComponent.cpp
+++ Source/RougeDemo/Private/Components/LockOnComponent.cpp
@@ -17,7 +17,7 @@ ULockOnComponent::ULockOnComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
 }
